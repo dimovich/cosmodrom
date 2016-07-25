0f80d9a80fbbecef7201dee63ab845b522210665
dimovich <dimovich@gmail.com>
2016-07-25 17:39:48 +0300

    some files added

---
+++ src/clj/cosmodrom/core.clj
@@ -0,0 +1,16 @@
+(ns cosmodrom.core 
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found files resources]]
+            [compojure.handler :refer [site]]
+            [shoreleave.middleware.rpc :refer [wrap-rpc]]))
+
+(defroutes handler
+  (GET "/" [] "Hello from Compojure!")  ;; for testing only
+  (files "/" {:root "target"})          ;; to serve static resources
+  (resources "/" {:root "target"})      ;; to serve anything else
+  (not-found "Page Not Found"))         ;; page not found
+
+(def app
+  (-> handler
+      (wrap-rpc)
+      (site)))
---
+++ src/cljs/cosmodrom/core.cljs
@@ -0,0 +1,5 @@
+(ns cosmodrom.core
+  (:require [cljsjs.fabric]
+            [cljsjs.pixi]
+            [thi.ng.color.core :as color]))
+
